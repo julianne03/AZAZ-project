e614714a0a6d24a34dece9066551a7d703929a62
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-09-16 01:46:57 +0900

    Category - home fragment 완성하기 #3

--- app/src/main/java/kr/hs/emirim/seungmin/javaproject_azaz/HomeFragment.java
+++ app/src/main/java/kr/hs/emirim/seungmin/javaproject_azaz/HomeFragment.java
@@ -2,12 +2,19 @@ package kr.hs.emirim.seungmin.javaproject_azaz;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
@@ -17,7 +24,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
