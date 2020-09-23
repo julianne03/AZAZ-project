0db5895fe7c16c95dd05111c96d62ece15403682
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-09-23 22:47:54 +0900

    review_list_item.xml id change

--- app/src/main/java/kr/hs/emirim/seungmin/javaproject_azaz/SetupActivity.java
+++ app/src/main/java/kr/hs/emirim/seungmin/javaproject_azaz/SetupActivity.java
@@ -5,7 +5,6 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
 
 import android.Manifest;
 import android.content.Intent;
