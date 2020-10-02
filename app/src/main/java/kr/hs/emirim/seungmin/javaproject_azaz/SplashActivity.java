645a3160b7ccf2a3ad788fb406db6eaa197a967d
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-10-02 16:10:17 +0900

    스플래쉬 화면 추가 (#24)

---
+++ app/src/main/java/kr/hs/emirim/seungmin/javaproject_azaz/SplashActivity.java
@@ -0,0 +1,30 @@
+package kr.hs.emirim.seungmin.javaproject_azaz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
