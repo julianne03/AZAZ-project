b9ff0498c59fe65aad98f11a8eb2b1f810d9189c
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-11-28 12:10:05 +0900

    recommend banner fragment add

--- app/src/main/java/kr/hs/emirim/seungmin/javaproject_azaz/Fragment/Reccomend1Fragment.java
+++ app/src/main/java/kr/hs/emirim/seungmin/javaproject_azaz/Fragment/Reccomend1Fragment.java
@@ -7,11 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import kr.hs.emirim.seungmin.javaproject_azaz.R;
+import kr.hs.emirim.seungmin.javaproject_azaz.imgBanerFragment.Banner1Fragment;
 
 
 public class Reccomend1Fragment extends Fragment {
+
+    ImageView imgBanner1;
+    Fragment Banner1Fragment;
 
     public Reccomend1Fragment() {
         // Required empty public constructor
@@ -21,6 +26,18 @@ public class Reccomend1Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reccomend1, container, false);
+        View v = inflater.inflate(R.layout.fragment_reccomend1, container, false);
+
+        imgBanner1 = v.findViewById(R.id.imgBanner1);
+        Banner1Fragment = new Banner1Fragment();
+
+        imgBanner1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.category_container, Banner1Fragment).commit();
+            }
+        });
+
+        return v;
     }
 }
